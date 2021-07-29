52770dd7dd91cb505e4b73f85caed207340918f1
Avaer Kazmer <a@modules.io>
2021-07-29 01:09:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import Head from 'next/head'
 import * as THREE from '../three.module.js';
 // import Image from 'next/image'
