731d267afd6fcfd663e6763d55fc7ab8af235e3a
Avaer Kazmer <a@modules.io>
2021-07-29 01:09:49 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,34 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const characters = [
+	{
+		name: 'Scillia',
+		imgSrc: 'characters/scillia.png',
+		class: 'Drop Hunter',
+	},
+	{
+		name: 'Drake',
+		imgSrc: 'characters/drake.png',
+		class: 'Hacker Supreme',
+	},
+	{
+		name: 'Hyacinth',
+		imgSrc: 'characters/hyacinth.png',
+		class: 'Beast Influencer',
+	},
+	{
+		name: 'Juniper',
+		imgSrc: 'characters/juniper.png',
+		class: 'Academy Engineer',
+	},
+	{
+		name: 'Anemone',
+		imgSrc: 'characters/anemone.png',
+		class: 'Lisk Witch',
+	},
+];
+
 export default function Home() {
   const state = useState();
 	
